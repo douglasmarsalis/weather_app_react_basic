ce4f6f632affb63c25c7407745176f7534cd1d26
Douglas <japanmarsalis@gmail.com>
2024-06-10 23:18:27 +0900

    Debugging issues with outdated node etc, Added font, image and some text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
 
-  const url = 'http://api.weatherapi.com/v1/current.json?key=62143c60b90d4257891103525241006&q=dallas'
+  //const url = 'http://api.weatherapi.com/v1/current.json?key=62143c60b90d4257891103525241006&q=dallas'
 
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className='container'>
+
+        {/* Top information */}
+       <div className='top'>
+        <div className='location'>
+          <p>Dallas</p>
+        </div>
+        <div className='temp'>
+            <h1>60°F</h1>
+        </div>
+        <div className='description'>Clouds</div>
+       </div>
+
+       {/* Bottom information */}
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>12 MPH</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
@@ -9,5 +9,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 
