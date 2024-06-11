6127a6c6d52e40b47a085730b2fc247c249824cf
Douglas <japanmarsalis@gmail.com>
2024-06-11 18:23:45 +0900

    Finished main code, had to change the API url, All seems to work well, Now just adjust to what I like for image and fonts

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
-  //const url = 'http://api.weatherapi.com/v1/current.json?key=62143c60b90d4257891103525241006&q=dallas'
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e4edabe0cf965862c45bd6d4d7037770`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      setLocation('');
+    }
+  };
 
   return (
     <div className="app">
+      <div className='search'>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyUp={searchLocation}
+          placeholder='Enter City Location'
+          type='text' />
+      </div>
       <div className='container'>
 
         {/* Top information */}
-       <div className='top'>
-        <div className='location'>
-          <p>Dallas</p>
-        </div>
-        <div className='temp'>
-            <h1>60°F</h1>
-        </div>
-        <div className='description'>Clouds</div>
-       </div>
-
-       {/* Bottom information */}
-        <div className='bottom'>
-          <div className='feels'>
-            <p className='bold'>65°F</p>
-            <p>Feels Like</p>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
           </div>
-          <div className='humidity'>
-            <p className='bold'>20%</p>
-            <p>Humidity</p>
+          <div className='temp'>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className='wind'>
-            <p className='bold'>12 MPH</p>
-            <p>Wind Speed</p>
+          <div className='description'>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
+        {/* Bottom information */}
+        {data.name != undefined &&
+          <div className='bottom'>
+            <div className='feels'>
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()} °F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className='humidity'>
+              {data.main ? <p className='bold'>{data.main.humidity.toFixed()} %</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className='wind'>
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
